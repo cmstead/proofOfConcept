5fecc3c6499b36e490084d56a12ae021d4ad2fb1
Chris Stead <cmstead@gmail.com>
2015-12-14 21:54:01 -0800

    Added watcher grunt task.

--- jadePrecompile/Gruntfile.js
+++ jadePrecompile/Gruntfile.js
@@ -1,15 +1,20 @@
 
-var jadeConfig = require('./grunt/jade.json');
+var jadeConfig = require('./grunt/jade.json'),
+    watchConfig = require('./grunt/watch.json');
 
 module.exports = function(grunt){
   
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        jade: jadeConfig
+        jade: jadeConfig,
+        watch: watchConfig
     });
 
     /* Load grunt task adapters */
 
     grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('build-jade', ['jade', 'watch:jade']);
 };
